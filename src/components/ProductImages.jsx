40a6aa4b867d6ccaa2e5122c7850c9f37132407d
redapy <baha.redaaaa@gmail.com>
2022-02-15 12:56:07 +0100

    add state to change images on click
    Co-authored-by: Ayu Adiati <retno.ayu98@gmail.com>

--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -3,18 +3,25 @@ import productThumbnail2 from '../assets/images/image-product-2-thumbnail.jpg'
 import productThumbnail3 from '../assets/images/image-product-3-thumbnail.jpg'
 import productThumbnail4 from '../assets/images/image-product-4-thumbnail.jpg'
 import product1 from '../assets/images/image-product-1.jpg'
+import product2 from '../assets/images/image-product-2.jpg'
+import product3 from '../assets/images/image-product-3.jpg'
+import product4 from '../assets/images/image-product-4.jpg'
+import { useState } from 'react'
+
 
 const ProductImages = () => {
+  const [image, setImage] = useState(product1);
+
   return (
     <>
       <div>
-        <img src={product1} alt="" />
+        <img src={image} alt="" />
       </div>
       <div className="thumbnails">
-        <img src={productThumbnail1} alt="" />
-        <img src={productThumbnail2} alt="" />
-        <img src={productThumbnail3} alt="" />
-        <img src={productThumbnail4} alt="" />
+        <img src={productThumbnail1} onClick={() => setImage(product1)} alt="" />
+        <img src={productThumbnail2} onClick={() => setImage(product2)} alt="" />
+        <img src={productThumbnail3} onClick={()  => setImage(product3)} alt="" />
+        <img src={productThumbnail4} onClick={() => setImage(product4)} alt="" />
       </div>
     </>
   )
