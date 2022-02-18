bd1c9ddf7695769fb746850db9362e8771a3a0bb
redapy <baha.redaaaa@gmail.com>
2022-02-18 12:47:05 +0100

    fix the first rendering of the big image
    Co-authored-by: Ayu Adiati <retno.ayu98@gmail.com>

--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 
 const ProductImages = ({images}) => {
-  const [image, setImage] = useState(images.product1);
+  const [image, setImage] = useState(images[0].productPic);
 
   return (
 			<>
