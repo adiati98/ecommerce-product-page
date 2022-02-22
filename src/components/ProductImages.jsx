adb04578a275058d4e64800635a355d81d675d99
redapy <baha.redaaaa@gmail.com>
2022-02-22 12:33:16 +0100

    add navigations arrows
    Co-authored-by: Ayu Adiati <retno.ayu98@gmail.com>

--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -1,10 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import nextArrow from "../assets/images/icon-next.svg";
+import previousArrow from "../assets/images/icon-previous.svg";
 
-const ProductImages = ({ images, setIsOpened }) => {
+const ProductImages = ({ images, setIsOpened, arrows }) => {
+  const [index, setIndex] = useState(0);
   const [image, setImage] = useState(images[0].productPic);
+
+  const moveToNextImage = () => {
+    if (index === 3) {
+      setIndex(0);
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  const moveToPreviousImage = () => {
+    if (index === 0) {
+      setIndex(3);
+    } else {
+      setIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  useEffect(() => {
+    setImage(images[index].productPic);
+  }, [index, images]);
 
   return (
     <>
+      {arrows && (
+        <img
+          src={previousArrow}
+          alt="previous arrow"
+          onClick={moveToPreviousImage}
+        />
+      )}
       <div>
         <img src={image} alt="" onClick={() => setIsOpened(true)} />
       </div>
@@ -17,6 +47,9 @@ const ProductImages = ({ images, setIsOpened }) => {
           />
         ))}
       </div>
+      {arrows && (
+        <img src={nextArrow} alt="next arrow" onClick={moveToNextImage} />
+      )}
     </>
   );
 };
