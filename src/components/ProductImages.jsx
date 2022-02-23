d251d83d85f6255a3e32681c1ee611f048609276
Retno Ayu Adiati <retno_ayu@hotmail.com>
2022-02-23 22:09:04 +0100

    Add alt texts in data.js * Add alt texts dynamically for product images and thumbnails * Add alt text for close icon at ImageModal component
    Co-authored-by: Reda Baha <baha.redaaaa@gmail.com>

--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -1,61 +1,63 @@
-import { useState } from "react";
-import nextArrow from "../assets/images/icon-next.svg";
-import previousArrow from "../assets/images/icon-previous.svg";
+import { useState } from 'react'
+import nextArrow from '../assets/images/icon-next.svg'
+import previousArrow from '../assets/images/icon-previous.svg'
 
 const ProductImages = ({ images, setIsOpened, arrows }) => {
-  const [index, setIndex] = useState(0);
-  const [image, setImage] = useState(images[0].productPic);
+	const [index, setIndex] = useState(0)
+	const [image, setImage] = useState(images[0].productPic)
+	const [altText, setAltText] = useState(images[0].altText)
 
-  const moveToNextImage = () => {
-    if (index === 3) {
-      return;
-    } else {
-      const newIndex = index + 1;
-      setIndex(newIndex);
-      setImage(images[newIndex].productPic);
-    }
-  };
+	const moveToNextImage = () => {
+		if (index === 3) {
+			return
+		} else {
+			const newIndex = index + 1
+			setIndex(newIndex)
+			setImage(images[newIndex].productPic)
+		}
+	}
 
-  const moveToPreviousImage = () => {
-    if (index === 0) {
-      return;
-    } else {
-      const newIndex = index - 1;
-      setIndex(newIndex);
-      setImage(images[newIndex].productPic);
-    }
-  };
+	const moveToPreviousImage = () => {
+		if (index === 0) {
+			return
+		} else {
+			const newIndex = index - 1
+			setIndex(newIndex)
+			setImage(images[newIndex].productPic)
+		}
+	}
 
-  return (
-    <>
-      {arrows && (
-        <img
-          src={previousArrow}
-          alt="previous arrow"
-          onClick={moveToPreviousImage}
-        />
-      )}
-      <div>
-        <img src={image} alt="" onClick={() => setIsOpened(true)} />
-      </div>
-      <div className="thumbnails">
-        {images.map((product, index) => (
-          <img
-            key={product.id}
-            src={product.thumbnail}
-            onClick={() => {
-              setImage(product.productPic);
-              setIndex(index);
-            }}
-            alt=""
-          />
-        ))}
-      </div>
-      {arrows && (
-        <img src={nextArrow} alt="next arrow" onClick={moveToNextImage} />
-      )}
-    </>
-  );
-};
+	return (
+		<>
+			{arrows && (
+				<img
+					src={previousArrow}
+					alt='previous arrow'
+					onClick={moveToPreviousImage}
+				/>
+			)}
+			<div>
+				<img src={image} alt={altText} onClick={() => setIsOpened(true)} />
+			</div>
+			<div className='thumbnails'>
+				{images.map((product, index) => (
+					<img
+						key={product.id}
+						src={product.thumbnail}
+						onClick={() => {
+							setImage(product.productPic)
+							setAltText(product.altText)
+							setIndex(index)
+						}}
+						alt={product.thumbnailAltText}
+					/>
+				))}
+			</div>
+			{arrows && (
+				<img src={nextArrow} alt='next arrow' onClick={moveToNextImage} />
+			)}
+		</>
+	)
+}
 
-export default ProductImages;
+export default ProductImages
