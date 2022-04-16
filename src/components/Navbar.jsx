7a602a8af4a8abeaed1ec0d57f7065c0d1243803
Retno Ayu Adiati <retno_ayu@hotmail.com>
2022-04-16 05:17:03 +0200

    Fix targeting cart and avatar with tab button
    Co-authored-by: Reda Baha <baha.redaaaa@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,56 +6,59 @@ import Quantity from "./Quantity";
 
 const NavBar = ({ setCartIsOpened, purchased }) => {
   return (
-    <Navbar expand="md" className="d-flex justify-content-start flex-nowrap">
-      <Navbar.Toggle aria-controls="offcanvasNavbar" className="ms-2" />
-      <Navbar.Brand className="mb-2 me-auto me-md-5 ps-1 align-self-center">
-        <img src={logo} alt="sneakers-logo" />
-      </Navbar.Brand>
-      <Nav className="me-auto nav-menu hide">
-        <Nav.Link href="#collection">Collections</Nav.Link>
-        <Nav.Link href="#men">Men</Nav.Link>
-        <Nav.Link href="#women">Women</Nav.Link>
-        <Nav.Link href="#about">About</Nav.Link>
-        <Nav.Link href="#contact">Contact</Nav.Link>
-      </Nav>
-      <Navbar.Offcanvas
-        id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel"
-      >
-        <Offcanvas.Header closeButton closeLabel />
-        <Offcanvas.Body>
-          <Nav className="d-flex align-items-md-center justify-content-evenly me-auto ms-md-5 nav-menu">
-            <Nav.Link href="#collection">Collections</Nav.Link>
-            <Nav.Link href="#men">Men</Nav.Link>
-            <Nav.Link href="#women">Women</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Offcanvas.Body>
-      </Navbar.Offcanvas>
-      <div className="d-flex align-items-center">
-        <Navbar.Text className="me-5 cart-wrapper">
-          <Quantity purchased={purchased} />
-          <img
-            src={cart}
-            alt="cart"
-            width="35px"
-            height="35px"
-            onClick={() => setCartIsOpened((prv) => !prv)}
-          />
-        </Navbar.Text>
-        <Navbar.Text>
-          <img
-            className="avatar"
-            src={avatar}
-            width="50px"
-            height="50px"
-            alt="avatar"
-          />
-        </Navbar.Text>
-      </div>
-    </Navbar>
-  );
+			<Navbar expand='md' className='d-flex justify-content-start flex-nowrap'>
+				<Navbar.Toggle aria-controls='offcanvasNavbar' className='ms-2' />
+				<Navbar.Brand className='mb-2 me-auto me-md-5 ps-1 align-self-center'>
+					<img src={logo} alt='sneakers-logo' />
+				</Navbar.Brand>
+				<Nav className='me-auto nav-menu hide'>
+					<Nav.Link href='#collection'>Collections</Nav.Link>
+					<Nav.Link href='#men'>Men</Nav.Link>
+					<Nav.Link href='#women'>Women</Nav.Link>
+					<Nav.Link href='#about'>About</Nav.Link>
+					<Nav.Link href='#contact'>Contact</Nav.Link>
+				</Nav>
+				<Navbar.Offcanvas
+					id='offcanvasNavbar'
+					aria-labelledby='offcanvasNavbarLabel'
+				>
+					<Offcanvas.Header closeButton closeLabel />
+					<Offcanvas.Body>
+						<Nav className='d-flex align-items-md-center justify-content-evenly me-auto ms-md-5 nav-menu'>
+							<Nav.Link href='#collection'>Collections</Nav.Link>
+							<Nav.Link href='#men'>Men</Nav.Link>
+							<Nav.Link href='#women'>Women</Nav.Link>
+							<Nav.Link href='#about'>About</Nav.Link>
+							<Nav.Link href='#contact'>Contact</Nav.Link>
+						</Nav>
+					</Offcanvas.Body>
+				</Navbar.Offcanvas>
+				<div className='d-flex align-items-center'>
+					<Navbar.Text className='me-5 cart-wrapper'>
+						<Quantity purchased={purchased} />
+						<img
+            className="p-1"
+							src={cart}
+							alt='cart'
+							width='40px'
+							height='40px'
+							tabindex='0'
+							onClick={() => setCartIsOpened((prv) => !prv)}
+						/>
+					</Navbar.Text>
+					<Navbar.Text>
+						<img
+							className='avatar'
+							src={avatar}
+							width='50px'
+							height='50px'
+							alt='avatar'
+							tabindex='0'
+						/>
+					</Navbar.Text>
+				</div>
+			</Navbar>
+		)
 };
 
 export default NavBar;
